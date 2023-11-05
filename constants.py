7cc0f8ede703310ce12ec69bd9e05fe7a390f67c
Akhilesh Shah <138904052+Akhushah@users.noreply.github.com>
2023-11-05 11:34:23 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,9 @@
+
+
+consumer_key = 'CYbS7f11AJPQtFmCXF12WiCcy'
+consumer_secret = 'VowKCxGPs1RXXAiYw2kqaFVOI7d4E0yP5YR57MYPGS1p02Lawo' 
+access_token = '1606859098771050498-Tds9FDEE6K4pVxFq5LYk8WCS2hDggy'
+access_token_secret = 'omRktJWgwjika0WyEBTx8cfFZnhx4SAaQFfzLNrc6AluF'
+
+
+num_of_tweets = int(300)
